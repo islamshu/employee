a7a1f68321b396208578a227f3f3b0d74953f14c
islamshu12 <islamshu12@gmail.com>
2022-06-16 14:38:29 +0300

    d

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1366,6 +1366,7 @@ src="https://www.facebook.com/tr?id=360462288775028&amp;ev=PageView&amp;noscript
 
           
           @foreach ($partners->chunk(4)as $key=>$item)
+          {{ dd($item) }}
           
           <div class="carousel-item active">
             @foreach ($item as $chunk)
