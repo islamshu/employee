9412a74f5d43f2937b2d9e05b8b5ff2f0697df8f
islamshu12 <islamshu12@gmail.com>
2022-07-14 15:34:07 +0300

    ب

--- resources/views/admin-dashbord/projects/edit.blade.php
+++ resources/views/admin-dashbord/projects/edit.blade.php
@@ -69,7 +69,7 @@
                                     <div class="row">
                                         <div class="col-md-6">
                                             <label>اضف صورة * </label>
-                                            <input type="file" name="image" class="form-control" required >
+                                            <input type="file" name="image" class="form-control"  >
                                         </div>
                                        
                                     </div>
