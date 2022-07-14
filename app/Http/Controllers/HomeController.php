c764f0357f1404d944fbabba27066434cd373d59
islamshu12 <islamshu12@gmail.com>
2022-07-14 16:18:44 +0300

    d

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,6 +33,7 @@ class HomeController extends Controller
 
         }
         $country = \Location::get($ip)->countryCode;
+        dd($country);
 
         return view('layouts.frontend')
         ->with('first_se',FirstSection::first())
