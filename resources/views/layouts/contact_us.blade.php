53f3943a6010e88f14799b7e21984d20de0f7170
islamshu12 <islamshu12@gmail.com>
2022-07-16 16:51:26 +0300

    f

--- resources/views/layouts/contact_us.blade.php
+++ resources/views/layouts/contact_us.blade.php
@@ -1115,11 +1115,30 @@
                                                                             </div>
 
                                                                         </div>
-                                                                        <button class="btn btn-info">انضم كتاجر</button>
+                                                                        <button class="btn btn-info" data-toggle="modal" data-target="#exampleModal">انضم كتاجر</button>
 
                                                                     </div>
                                                                 </div>
                                                             </div>
+                                                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                                <div class="modal-dialog" role="document">
+                                                                  <div class="modal-content">
+                                                                    <div class="modal-header">
+                                                                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                        <span aria-hidden="true">&times;</span>
+                                                                      </button>
+                                                                    </div>
+                                                                    <div class="modal-body">
+                                                                      ...
+                                                                    </div>
+                                                                    <div class="modal-footer">
+                                                                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                                      <button type="button" class="btn btn-primary">Save changes</button>
+                                                                    </div>
+                                                                  </div>
+                                                                </div>
+                                                              </div>
                                                             <div class="wpb_column vc_column_container vc_col-sm-6">
                                                                 <div class="vc_column-inner ">
                                                                     <div class="wpb_wrapper">
