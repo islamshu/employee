c7be053413302df035a7f9ea12e2e63bae593df2
islamshu12 <islamshu12@gmail.com>
2022-07-16 16:53:20 +0300

    d

--- resources/views/layouts/contact_us.blade.php
+++ resources/views/layouts/contact_us.blade.php
@@ -1115,28 +1115,28 @@
                                                                             </div>
 
                                                                         </div>
-                                                                        <button class="btn btn-info" data-toggle="modal" data-target="#exampleModal">انضم كتاجر</button>
+                                                                        <button class="btn btn-info" data-toggle="modal" data-target="#myModal">انضم كتاجر</button>
 
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                                <div class="modal-dialog" role="document">
+                                                            <div class="modal fade" id="myModal" role="dialog">
+                                                                <div class="modal-dialog">
+                                                                
+                                                                  <!-- Modal content-->
                                                                   <div class="modal-content">
                                                                     <div class="modal-header">
-                                                                      <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                                        <span aria-hidden="true">&times;</span>
-                                                                      </button>
+                                                                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                                      <h4 class="modal-title">Modal Header</h4>
                                                                     </div>
                                                                     <div class="modal-body">
-                                                                      ...
+                                                                      <p>Some text in the modal.</p>
                                                                     </div>
                                                                     <div class="modal-footer">
-                                                                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                                      <button type="button" class="btn btn-primary">Save changes</button>
+                                                                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                                                                     </div>
                                                                   </div>
+                                                                  
                                                                 </div>
                                                               </div>
                                                             <div class="wpb_column vc_column_container vc_col-sm-6">
