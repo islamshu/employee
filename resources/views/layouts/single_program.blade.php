2f70c3178ae0f20ca8687ec50a41348052708e2a
islamshu12 <islamshu12@gmail.com>
2022-07-16 14:35:13 +0300

    d

--- resources/views/layouts/single_program.blade.php
+++ resources/views/layouts/single_program.blade.php
@@ -5,6 +5,77 @@
     <meta charset="UTF-8">
 
     <style>
+                 .wrapper-cover {
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    height: 100%;
+    background: radial-gradient(#69489D, #DE4F76) !important;
+    position: fixed;
+    top: 0;
+    right: 0;
+    z-index: 1555;
+}
+.wrapper {
+    width: 250px;
+    height: 80px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.circle{
+    width:30px;
+    height:30px;
+    position: absolute;
+    border-radius: 50%;
+    background-color: #fff;
+    left:10%;
+    transform-origin: 50%;
+    animation: circle .5s alternate infinite ease;
+}
+
+@keyframes circle{
+    0%{
+        top:60px;
+        height:5px;
+        border-radius: 50px 50px 25px 25px;
+        transform: scaleX(1.7);
+    }
+    40%{
+        height:20px;
+        border-radius: 50%;
+        transform: scaleX(1);
+    }
+    100%{
+        top:0%;
+    }
+}
+.circle:nth-child(2){
+    left:45%;
+    animation-delay: .2s;
+}
+.circle:nth-child(3){
+    left:auto;
+    right:15%;
+    animation-delay: .3s;
+}
+.shadow{
+    width:30px;
+    height:4px;
+    border-radius: 50%;
+    background-color: rgba(0,0,0,.5);
+    position: absolute;
+    top:62px;
+    transform-origin: 50%;
+    z-index: -1;
+    left:15%;
+    filter: blur(1px);
+    animation: shadow .5s alternate infinite ease;
+}
    
 
         .vc_custom_1601577362547 {
@@ -798,9 +869,9 @@
 <body class="rtl home page-template-default page page-id-1016 wpb-js-composer js-comp-ver-6.6.0 vc_responsive">
 
 
-    <div id="page-preloader">
+    {{-- <div id="page-preloader">
         <div class="page-preloader-spin"></div>
-    </div>
+    </div> --}}
 
 
     <div id="page" class="layout-fullwidth header-style-1">
@@ -1328,6 +1399,24 @@
             <!-- .footer-widget-area -->
         </div>
     </footer>
+    <div class="wrapper-cover">
+
+        <div class="wrapper">
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="circle"></div>
+            <div class="shadow"></div>
+            <div class="shadow"></div>
+            <div class="shadow"></div>
+            @if(get_lang() == 'ar')
+            <span>... جاري التحميل  </span>
+            @else
+            <span> Loading ...  </span>
+
+            @endif
+        </div>
+
+    </div>
     <!-- #colophon -->
 
 
@@ -1390,6 +1479,12 @@
     <script type='text/javascript'
         src='{{ asset('wp_content/themes/thegem/js/fancyBox/jquery.fancybox-init647e.js?ver=5.5.9') }}'
         id='fancybox-init-script-js'></script>
+        <script>
+            $(window).on("load", function() {
+                  $('.wrapper-cover').fadeOut("slow"); 
+      
+              });
+         </script>
     <script type='text/javascript' id='leadin-script-loader-js-js-extra'>
         /* <![CDATA[ */
         var leadin_wordpress = {
