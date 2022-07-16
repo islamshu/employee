88321d7507beb4bc61ff580d42dcbd2fc95aa654
islamshu12 <islamshu12@gmail.com>
2022-07-16 14:10:39 +0300

    d

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -27,6 +27,88 @@
     justify-content: center;
     align-items: center;
 }
+.circle{
+    width:30px;
+    height:30px;
+    position: absolute;
+    border-radius: 50%;
+    background-color: #fff;
+    left:10%;
+    transform-origin: 50%;
+    animation: circle .5s alternate infinite ease;
+}
+
+@keyframes circle{
+    0%{
+        top:60px;
+        height:5px;
+        border-radius: 50px 50px 25px 25px;
+        transform: scaleX(1.7);
+    }
+    40%{
+        height:20px;
+        border-radius: 50%;
+        transform: scaleX(1);
+    }
+    100%{
+        top:0%;
+    }
+}
+.circle:nth-child(2){
+    left:45%;
+    animation-delay: .2s;
+}
+.circle:nth-child(3){
+    left:auto;
+    right:15%;
+    animation-delay: .3s;
+}
+.shadow{
+    width:30px;
+    height:4px;
+    border-radius: 50%;
+    background-color: rgba(0,0,0,.5);
+    position: absolute;
+    top:62px;
+    transform-origin: 50%;
+    z-index: -1;
+    left:15%;
+    filter: blur(1px);
+    animation: shadow .5s alternate infinite ease;
+}
+
+@keyframes shadow{
+    0%{
+        transform: scaleX(1.5);
+    }
+    40%{
+        transform: scaleX(1);
+        opacity: .7;
+    }
+    100%{
+        transform: scaleX(.2);
+        opacity: .4;
+    }
+}
+.shadow:nth-child(4){
+    left: 45%;
+    animation-delay: .2s
+}
+.shadow:nth-child(5){
+    left:auto;
+    right:15%;
+    animation-delay: .3s;
+}
+.wrapper span{
+    position: absolute;
+    top:75px;
+    font-size: 26px;
+    letter-spacing: 5px;
+    color: #fff;
+    left:10%;
+    font-weight: 600;
+}
+
         .tgpli-background-inited {
             background-image: none !important;
         }
@@ -891,19 +973,7 @@ src="https://www.facebook.com/tr?id=360462288775028&amp;ev=PageView&amp;noscript
     {{-- <div id="page-preloader">
         <div class="page-preloader-spin"></div>
     </div> --}}
-    <div class="wrapper-cover">
-
-        <div class="wrapper">
-            <div class="circle"></div>
-            <div class="circle"></div>
-            <div class="circle"></div>
-            <div class="shadow"></div>
-            <div class="shadow"></div>
-            <div class="shadow"></div>
-            <span>Loading...</span>
-        </div>
-
-    </div>
+   
     {{-- <div id="loading">
         <img id="loading-image" src="https://cdn1.iconfinder.com/data/icons/system-preferences/320/loading_loader_progress_wait_spinner_load-512.png" alt="Loading..." />
       </div> --}}
@@ -1910,6 +1980,19 @@ src="https://www.facebook.com/tr?id=360462288775028&amp;ev=PageView&amp;noscript
                 <!-- .footer-widget-area -->
             </div>
         </footer>
+        <div class="wrapper-cover">
+
+            <div class="wrapper">
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="circle"></div>
+                <div class="shadow"></div>
+                <div class="shadow"></div>
+                <div class="shadow"></div>
+                <span>Loading...</span>
+            </div>
+    
+        </div>
         <!-- #colophon -->
 
 
