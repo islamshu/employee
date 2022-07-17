fcc42e43acf3821e5b7172aeb41e0ca8e3147a44
islamshu12 <islamshu12@gmail.com>
2022-07-17 19:29:45 +0300

    g

--- resources/views/admin-dashbord/partials/aside.blade.php
+++ resources/views/admin-dashbord/partials/aside.blade.php
@@ -1,35 +1,60 @@
 <div class="main-menu menu-fixed menu-light menu-accordion    menu-shadow " data-scroll-to-active="true">
     <div class="main-menu-content">
-      <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-        <li class=" nav-item"><a href="{{ route('generalinfo.index') }}"><i class="la la-cog"></i><span class="menu-title" data-i18n="nav.dash.main">الإعدادات العامة</span></a>  
-        </li>
-        <li class=" nav-item"><a href="{{ route('first_section') }}" ><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main"> القسم الأول</span></a>
-        </li>
-        <li class=" nav-item"><a href="{{ route('partners') }}" ><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main"> شركائنا </span></a>
-        </li>
-        <li class=" nav-item"><a href="{{ route('third_section') }}" ><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main"> القسم الثالث </span></a>
-        </li>
-        <li class=" nav-item"><a href="{{ route('programs.index') }}" ><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main"> برامجنا  </span></a>
-        </li>
-        <li class=" nav-item"><a href="{{ route('statistic.index') }}" ><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main"> الاحصائيات  </span></a>
-        </li>
-        <li class=" nav-item"><a href="{{ route('contacts.index') }}" ><i class="la la-home"></i><span class="menu-title" data-i18n="nav.dash.main"> طلبات المراسلة  </span></a>
-        </li>
-     
-        <li class="nav-item ">
-          <a class="menu-item" href="#" data-i18n="nav.menu_levels.second_level_child.main">
-              <i class="la la-thumb-tack"></i>
-              <span class="menu-title" data-i18n="nav.page_layouts.main">الترجمة</span>
-          </a>
+        <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+            <li class=" nav-item"><a href="{{ route('generalinfo.index') }}"><i class="la la-cog"></i><span
+                        class="menu-title" data-i18n="nav.dash.main">الإعدادات العامة</span></a>
+            </li>
+            <li class=" nav-item"><a href="{{ route('first_section') }}"><i class="la la-home"></i><span
+                        class="menu-title" data-i18n="nav.dash.main"> القسم الأول</span></a>
+            </li>
+            <li class=" nav-item"><a href="{{ route('partners') }}"><i class="la la-home"></i><span class="menu-title"
+                        data-i18n="nav.dash.main"> شركائنا </span></a>
+            </li>
+            <li class=" nav-item"><a href="{{ route('third_section') }}"><i class="la la-home"></i><span
+                        class="menu-title" data-i18n="nav.dash.main"> القسم الثالث </span></a>
+            </li>
+            <li class=" nav-item"><a href="{{ route('programs.index') }}"><i class="la la-home"></i><span
+                        class="menu-title" data-i18n="nav.dash.main"> برامجنا </span></a>
+            </li>
+            <li class=" nav-item"><a href="{{ route('statistic.index') }}"><i class="la la-home"></i><span
+                        class="menu-title" data-i18n="nav.dash.main"> الاحصائيات </span></a>
+            </li>
+            <li class=" nav-item"><a href="{{ route('contacts.index') }}"><i class="la la-home"></i><span
+                        class="menu-title" data-i18n="nav.dash.main"> طلبات المراسلة </span></a>
+            </li>
+            <li class="nav-item ">
+              <a class="menu-item" href="#" data-i18n="nav.menu_levels.second_level_child.main">
+                  <i class="la la-thumb-tack"></i>
+                  <span class="menu-title" data-i18n="nav.page_layouts.main">المقالات</span>
+              </a>
 
-      <ul class="menu-content" style="">
-          <li class="is-shown"><a class="menu-item" href="{{ route('show_translate','ar') }}" data-i18n="nav.menu_levels.second_level_child.third_level">ترجمة الخاصة باللغة العربية  </a>
+              <ul class="menu-content" style="">
+                  <li class="is-shown"><a class="menu-item" href="{{ route('blogs.index') }}"
+                          data-i18n="nav.menu_levels.second_level_child.third_level">جميع المقالات </a>
+                  </li>
+                  <li class="is-shown"><a class="menu-item" href="{{ route('blogs.create') }}"
+                          data-i18n="nav.menu_levels.second_level_child.third_level">  اضف جديد 
+                      </a>
+                  </li>
+              </ul>
           </li>
-          <li class="is-shown"><a class="menu-item" href="{{ route('show_translate','en') }}" data-i18n="nav.menu_levels.second_level_child.third_level">الترجمة الخاصة باللغة الانجليزية </a>
-          </li>
+            <li class="nav-item ">
+                <a class="menu-item" href="#" data-i18n="nav.menu_levels.second_level_child.main">
+                    <i class="la la-thumb-tack"></i>
+                    <span class="menu-title" data-i18n="nav.page_layouts.main">الترجمة</span>
+                </a>
+
+                <ul class="menu-content" style="">
+                    <li class="is-shown"><a class="menu-item" href="{{ route('show_translate', 'ar') }}"
+                            data-i18n="nav.menu_levels.second_level_child.third_level">ترجمة الخاصة باللغة العربية </a>
+                    </li>
+                    <li class="is-shown"><a class="menu-item" href="{{ route('show_translate', 'en') }}"
+                            data-i18n="nav.menu_levels.second_level_child.third_level">الترجمة الخاصة باللغة الانجليزية
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
         </ul>
-      </li>
-
-      </ul>
     </div>
-  </div>
+</div>
